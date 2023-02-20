e056d7b39e74004a649bdebe9d62d5b4bcdb9948
Jay <jayzawrotny@gmail.com>
2023-02-20 14:46:49 -0500

    Removed node-fetch

--- src/gracie/projects/core.cljs
+++ src/gracie/projects/core.cljs
@@ -1,17 +1,14 @@
 (ns gracie.projects.core
-  (:require
-   [promesa.core :as p]
-   [notion.api :as notion]
-   [framework.env :as env]
-   [framework.utils :as u]
-   ["node-fetch$default" :as fetch]
-   ))
+  (:require [promesa.core :as p]
+            [notion.api :as notion]
+            [framework.env :as env]
+            [framework.utils :as u]))
+
 
 
 (defn format-pdf
   [file]
-  {:name (get file :name)
-   :url  (get-in file [:file :url])})
+  {:name (get file :name), :url (get-in file [:file :url])})
 
 (defn normalize-thumbnail
   [video]
@@ -22,39 +19,29 @@
   (let [vimeo-api-url (str "https://vimeo.com/api/oembed.json?url="
                            (js/encodeURIComponent url)
                            "&width=1280&height=720")
-        params (clj->js {:headers {"Referer" "https://gracieanimator.squarespace.com"}})]
-    (-> (p/-> (fetch vimeo-api-url params)
+        params (clj->js {:headers
+                           {"Referer"
+                              "https://gracieanimator.squarespace.com"}})]
+    (-> (p/-> (js/fetch vimeo-api-url params)
               (.json)
               (js->clj :keywordize-keys true)
               (normalize-thumbnail))
-        (p/catch
-            (fn [error]
-              (js/console.error error)
-              {})))))
+        (p/catch (fn [error] (js/console.error error) {})))))
 
-(defn fetch-video
-  [vimeo-url]
-  (if vimeo-url
-    (fetch-vimeo-oembed vimeo-url)
-    nil))
+(defn fetch-video [vimeo-url] (if vimeo-url (fetch-vimeo-oembed vimeo-url) nil))
 
 (defn fetch-speakerdeck-oembed
   [url]
   (let [vimeo-api-url (str "https://speakerdeck.com/oembed.json?url="
                            (js/encodeURIComponent url))]
-    (-> (p/-> (fetch vimeo-api-url)
+    (-> (p/-> (js/fetch vimeo-api-url)
               (.json)
               (js->clj :keywordize-keys true))
-        (p/catch
-            (fn [error]
-              (js/console.error error)
-              {})))))
+        (p/catch (fn [error] (js/console.error error) {})))))
 
 (defn fetch-speakerdeck
   [speakerdeck-url]
-  (if speakerdeck-url
-    (fetch-speakerdeck-oembed speakerdeck-url)
-    nil))
+  (if speakerdeck-url (fetch-speakerdeck-oembed speakerdeck-url) nil))
 
 (defn format-project
   [project]
@@ -65,26 +52,26 @@
     ;; Fetch these resources in parallel
     (p/let [[video speakerdeck] (p/all [(fetch-video vimeo-url)
                                         (fetch-speakerdeck speakerdeck-url)])]
-      {:id         (get project :id)
-       :uid        (get-in fields [:uid :formula :string])
-       :pdfs       (->> (get-in fields [:pdf :files])
-                        (map format-pdf))
+      {:id (get project :id),
+       :uid (get-in fields [:uid :formula :string]),
+       :pdfs (->> (get-in fields [:pdf :files])
+                  (map format-pdf)),
        :thumbnail (->> (get-in fields [:thumbnail :files])
                        (map #(get-in % [:file :url]))
-                       (first))
-       :image      (->> (get-in fields [:image :files])
-                        (map #(get-in % [:file :url]))
-                        (first))
-       :vimeo-url  vimeo-url
-       :video      video
-       :speakerdeck-url speakerdeck-url
-       :speakerdeck speakerdeck
-       :type       type
-       :category   (get-in fields [:category :select :name])
-       :tags       (->> (get-in fields [:tags :multi-select])
-                        (map #(select-keys % [:id :name])))
-       :featured   (get-in fields [:featured :checkbox])
-       :title      (get-in fields [:name :title 0 :text :content])
+                       (first)),
+       :image (->> (get-in fields [:image :files])
+                   (map #(get-in % [:file :url]))
+                   (first)),
+       :vimeo-url vimeo-url,
+       :video video,
+       :speakerdeck-url speakerdeck-url,
+       :speakerdeck speakerdeck,
+       :type type,
+       :category (get-in fields [:category :select :name]),
+       :tags (->> (get-in fields [:tags :multi-select])
+                  (map #(select-keys % [:id :name]))),
+       :featured (get-in fields [:featured :checkbox]),
+       :title (get-in fields [:name :title 0 :text :content]),
        :updated-at (get project :last-edited-time)})))
 
 (defn format-projects
@@ -101,52 +88,40 @@
 (defn type-order
   [[type _projects]]
   (case type
-    "Storyboards"        0
-    "Illustrations"      1
+    "Storyboards" 0
+    "Illustrations" 1
     "Sketchbook Samples" 2
-    "Comics"             3
+    "Comics" 3
     99))
 
-(defn sort-types
-  [projects-by-type]
-  (sort-by type-order projects-by-type))
+(defn sort-types [projects-by-type] (sort-by type-order projects-by-type))
 
-(defn db->categories
-  [db]
-  (get-in db [:properties :category :select :options]))
+(defn db->categories [db] (get-in db [:properties :category :select :options]))
 
 (defn find-category
   [categories slug]
-  (some #(when (= (u/slugify (:name %)) slug)
-           (:name %)) categories))
+  (some #(when (= (u/slugify (:name %)) slug) (:name %)) categories))
 
 (defn sort-newest-first
   [projects]
   (sort-by :updated-at #(compare %2 %1) projects))
 
-(defn group-by-type
-  [projects]
-  (group-by :type projects))
+(defn group-by-type [projects] (group-by :type projects))
 
-(defn group-by-category
-  [projects]
-  (group-by :category projects))
+(defn group-by-category [projects] (group-by :category projects))
 
 (defn format-page
   [page]
   (let [props (get page :properties {})]
-    {:id (get page :id)
-     :slug (get-in props [:url-friendly-name :rich-text 0 :text :content])
+    {:id (get page :id),
+     :slug (get-in props [:url-friendly-name :rich-text 0 :text :content]),
      :title (get-in props [:name :title 0 :text :content])}))
 
 (defn fetch-pages
   []
   (p/->> (notion/fetch-db-entries
-          {:db-id (env/required "CMS_PAGES_ID")
-           :filter {:and [{:property "Published"
-                           :checkbox {:equals true}}
-                          ]}
-           :sorts [{:property "Order"
-                    :direction "ascending"}]})
+           {:db-id (env/required "CMS_PAGES_ID"),
+            :filter {:and [{:property "Published", :checkbox {:equals true}}]},
+            :sorts [{:property "Order", :direction "ascending"}]})
          (map format-page)
          (p/all)))
--- workers/download.cljs
+++ workers/download.cljs
@@ -1,28 +1,19 @@
 (ns workers.download
-  (:require
-   [clojure.pprint :refer [pprint]]
-   [promesa.core :as p]
-   [clojure.string :as s]
-   ["path" :as path]
-   ["node-fetch$default" :as fetch]
-   ["fs" :as fs]))
+  (:require [clojure.pprint :refer [pprint]]
+            [promesa.core :as p]
+            [clojure.string :as s]
+            ["path" :as path]
+            ["fs" :as fs]))
 
 (defn normalize-filename
   [filename basename]
-  (cond (and filename
-             (s/includes? filename "."))
-        filename
-
-        filename
-        (str filename (.extname path basename))
-
-        :else
-        basename))
+  (cond (and filename (s/includes? filename ".")) filename
+        filename (str filename (.extname path basename))
+        :else basename))
 
 
-(let [{:keys [url dir filename root]} (js->clj
-                                       (js/JSON.parse (nth js/process.argv 3))
-                                       :keywordize-keys true)
+(let [{:keys [url dir filename root]
+        (js->clj (js/JSON.parse (nth js/process.argv 3)) :keywordize-keys true)}
       url-obj (js/URL. url)
       url-path (.-pathname url-obj)
       base (normalize-filename filename (.basename path url-path))
@@ -30,16 +21,13 @@
       dest-url (.join path dir base)]
   (.mkdirSync fs (.join path root dir) #js {:recursive true})
   (let [dest-stream (.createWriteStream fs dest-file)]
-    (p/let [res (fetch url)]
+    (p/let [res (js/fetch url)]
       (-> (js/Promise.
-             (fn [resolve]
-               (.once
-                dest-stream "finish" (fn []
-                                       (resolve (str "/" dest-url))))
-               (.pipe (.-body res) dest-stream)))
+            (fn [resolve]
+              (.once dest-stream "finish" (fn [] (resolve (str "/" dest-url))))
+              (.pipeTo (.-body res) dest-stream)))
           (p/then println)
-          (p/catch
-              (fn [err]
-                (js/console.error "Failed downloading" url)
-                (js/console.error err)
-                (js/throw err)))))))
+          (p/catch (fn [err]
+                     (js/console.error "Failed downloading" url)
+                     (js/console.error err)
+                     (js/throw err)))))))
