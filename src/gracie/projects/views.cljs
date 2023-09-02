1c2c5321adde18b4fdf3288caff32a9333209bb2
Jay <jayzawrotny@gmail.com>
2023-09-02 01:28:20 -0400

    Cleanup project-thumb component

--- src/gracie/projects/views.cljs
+++ src/gracie/projects/views.cljs
@@ -6,13 +6,11 @@
   [{:keys [url project]} & children]
   [:div.max-w-xs.w-full
    [:div.text-center
-    [:div.relative.overflow-hidden {:style {:height "13.5rem"}}
-     [:a.absolute.left-0.right-0.top-0.bottom-0.block
-      {:href url,
-       :style {:background-image
-                 (str "url('" (:thumbnail project) "')"),
-               :background-repeat "no-repeat",
-               :background-size "cover",
-               :background-position "center"}}]]
-    [:h2.uppercase.text-lg.font-body.font-light
-     (into [:a {:href url}] children)]]])
+    [:div
+     [:a.block
+      {:href url}
+      [:img.object-cover.h-64.w-full
+       {:src (:thumbnail project)}]
+      (into
+        [:span.uppercase.text-lg.font-body.font-light
+        children])]]]])
